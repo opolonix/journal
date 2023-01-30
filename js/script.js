a6f809d44287e9d9c6c774a9dc0eda5b3c7d8cd7
MISHA <misha90638@gmail.com>
2023-01-31 02:03:04 +0300

    слайдер дз работает
    сделал возможность открывать и закрывать плашку с дз, первый js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+class Unit{
+    constructor(){
+        this.__global_var = {}
+    }
+    Global(name){
+        return this.__global_var[name]
+    }
+    setGlobal(name, data){
+        this.__global_var[name] = data
+    }
+}
+un = new Unit();
+
+un.setGlobal("open_homework_win", false)
+var homework_win = document.querySelector(".slide_container")
+
+
+document.addEventListener("click", (event)=>{
+    if (un.Global("open_homework_win") == false){open_homework()}
+    else{close_homework()}
+})
+
+
+
+function open_homework(){
+    homework_win.classList.remove("close")
+    un.setGlobal("open_homework_win", true)
+}
+function close_homework(){
+    homework_win.classList.add("close")
+    un.setGlobal("open_homework_win", false)
+}
