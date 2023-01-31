156dd10eee04fee12198bb7604ab34cb41f7a701
MISHA <misha90638@gmail.com>
2023-02-01 01:50:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,11 +31,23 @@ function close_homework(){
     un.setGlobal("open_homework_win", false)
 }
 
-var lastY = 1;
-document.addEventListener("touchmove", function (event) 
-    {
-        if((lastY-event.touches[0].clientY) < 0 && event.cancelable){event.preventDefault(); event.stopPropagation();};
-        lastY = event.touches[0].clientY;
-    },
-    {passive: false}
-);
+// var lastY = 1;
+// document.addEventListener("touchmove", function (event) 
+//     {
+//         if((lastY-event.touches[0].clientY) < 0 && event.cancelable){event.preventDefault(); event.stopPropagation();};
+//         lastY = event.touches[0].clientY;
+//     },
+//     {passive: false}
+// );
+
+
+// var x, y = 0
+// main_card = document.querySelector(".card.main")
+// main_card = main_card.getBoundingClientRect()
+// document.addEventListener("touchstart", function (event) 
+//     {   
+//         console.log(event.touches[0].clientX, event.touches[0].clientY);
+//     },
+//     {passive: false}
+// );
+
