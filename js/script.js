41668aebc15b78f74a3939db887c62fddb6c07d3
MISHA <misha90638@gmail.com>
2023-02-02 01:58:45 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ class Unit{
     setGlobal(name, data){
         this._global_var[name] = data
     }
-    setTimeTable(file){querytimeTable(file)}
+    setTimeTable(file){querytimeTable(file, new Date)}
 }
 
 un = new Unit();
@@ -36,9 +36,38 @@ document.querySelector('.navigation_bar .btns button').addEventListener("click",
 function open_homework(){homework_win.classList.remove("close"); un.setGlobal("open_homework_win", true)}
 function close_homework(){homework_win.classList.add("close"); un.setGlobal("open_homework_win", false)}
 
-function querytimeTable(data){
-    console.log(data);
+function querytimeTable(data, time){
+    day_num = time.getDay()-1
+    day_name = data[day_num]['name']
+    day_date = formatDate(time)
+    week = Math.round((time - new Date(time.getFullYear(), time.getMonth(), 0).getTime()) / (1000 * 60 * 60 * 24 * 7));
+    if(week % 2){week = 'blue'}
+    else{week = 'red'}
+    // console.log(day_name, day_date, week);
+
+    card = document.createElement('div')
+    card.classList.add('card')
+    card.classList.add('main')
+
+    week_color = document.createElement('div')
+    week_color.classList.add('weak_color')
+    week_color.classList.add(week)
+    card.append(week_color)
+
+    cards_container = document.querySelector('.cards_container')
+    //cards_container.append(card)
+
+    console.log(card);
 }
+
+function formatDate(date) {
+    var dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+    var mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+    var yy = date.getFullYear()
+    return dd + '.' + mm + '.' + yy;
+  }
 // var lastY = 1;
 // document.addEventListener("touchmove", function (event) 
 //     {
